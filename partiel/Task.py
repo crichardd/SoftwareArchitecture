9bf3a1b6e22051469e038397a88ceda82baab9d1
Barlords <basile.pulin@gmail.com>
2024-06-25 17:31:26 +0200

    initial commit

---
+++ partiel/Task.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+class Task:
+    def __init__(self, name, date=None, finished=False):
+        """
+        Initialise une nouvelle tâche.
+
+        :param name: Le nom de la tâche.
+        :param finished: Statut d'achèvement de la tâche (par défaut False).
+        """
+        self.name = name
+        self.date = date if date is not None else datetime.now()
+        self.finished = finished
+
+    def mark_finished(self):
+        self.finished = True
+
+    def __str__(self):
+        """
+        Renvoie une représentation en chaîne de la tâche.
+        """
+        return f"Task(name={self.name}, finished={self.finished})"
